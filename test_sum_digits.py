9f3eb4ee6f43a44e8111784aec980d91a37d4a0c
Muhammed <mohamed.hoda1999@gmail.com>
2026-03-09 13:03:12 +0200

    Add new Test Cases

--- test_sum_digits.py
+++ test_sum_digits.py
@@ -19,3 +19,20 @@ def test_digits_only_returns_correct_sum():
 
 def test_special_characters_are_ignored():
     assert sum_of_digits("!@#4$%5") == 9
+
+def test_negative_numbers():
+    assert sum_of_digits("10 and -3 and 7") == 14
+
+def test_large_numbers():
+    assert sum_of_digits("1d2f3add45678afef9012feMuhammedafa3456faeea789gsSRrg0") == 45
+
+def test_zero_values(): 
+    assert sum_of_digits("0a0b0c") == 0
+
+def test_newline_separated(self):
+    assert sum_of_digits("4\n8\n15\n16\n23\n42") == 108
+
+def test_tab_separated(self):
+    assert sum_of_digits("10\t20\t30") == 60
+
+
